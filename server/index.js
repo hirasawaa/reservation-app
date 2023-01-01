2c23d2003c60a8362867b5e397e4654dc61c9386
hirasawaa <ahikoki77@gmail.com>
2023-01-01 16:57:59 +0900

    Heroku用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.set('strictQuery', false);
 mongoose.connect(config.DB_URI, {
@@ -11,24 +12,30 @@ mongoose.connect(config.DB_URI, {
     useUnifiedTopology: true
 }).then(
     () => {
-        const fakeDb = new FakeDb()
-        fakeDb.initDb()
+        if (process.env.NODE_ENV !== 'production') {
+            const fakeDb = new FakeDb()
+            // fakeDb.initDb()
+        }
     }
 )
 
 
 const app = express()
 
-app.use('/api/v1/products',productRoutes)
+app.use('/api/v1/products', productRoutes)
 
-// app.get('/products', function (req, res) {
-//     res.json({ 'success': true })
-// })
+if (process.env.NODE_ENV === 'production') {
+    const appPath = path.join(__dirname, '..', 'dist', 'reservation-app')
+    app.use(express.static(appPath))
+    app.get("*", function (req, res) {
+        res.sendFile(path.resolve(appPath, 'index.html'))
+    })
+}
+
+
 
 const PORT = process.env.PORT || '3001'
 
 app.listen(PORT, function () {
     console.log('I am running!!!')
 })
-
-// mongodb+srv://test:<password>@cluster0.yx5etoa.mongodb.net/?retryWrites=true&w=majority
