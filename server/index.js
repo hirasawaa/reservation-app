a564e19a2896cc8835588786bfeaa76241f582f4
hirasawaa <105769118+hirasawaa@users.noreply.github.com>
2023-01-08 17:12:31 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const bodyParser =require('body-parser')
-const config = require('./config/dev')
+const config = require('./config')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
